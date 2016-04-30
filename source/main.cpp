692bc90502b9083f53db3fae1021ea94f6ceda94
Matthew French <matthewf@santoslab.org>
2016-04-29 21:47:28 -0500

    Preparing to try to make it run

--- source/main.cpp
+++ source/main.cpp
@@ -4,14 +4,21 @@
 #define cimg_use_png
 #include "lib/CImg.h"
 #import "Utility.h"
+#import "DetectionManager.h"
 
 using namespace cimg_library; 
 
 int main() {
   // call a function in another file
   //myPrintHelloMake();
-  CImg<unsigned char> image = CImg("Test1.png");
+  //CImg<unsigned char> image = CImg("Test1.png");
+
+  ImageData image = loadImage("Test1.png");
+
+
   printf("Loaded image!\n");
+
+
 
   return(0);
 }
