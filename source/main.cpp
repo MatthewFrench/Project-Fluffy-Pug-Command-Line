a39393a9367f35b18e947c7a73adc4bf6636fcd0
Matthew French <matthewf@santoslab.org>
2016-05-01 01:48:18 -0500

    Made it scan all the test images

--- source/main.cpp
+++ source/main.cpp
@@ -6,11 +6,52 @@
 
 using namespace cimg_library; 
 
+double runTestAndOutput(string folder, string pngImageName);
 void printDetected(DetectionManager* detectionManager);
 void outputImage(ImageData image, DetectionManager* detection, CImg<unsigned char> render, string name);
 
 int main() {
-	ImageData image = loadImage("Test1.png");
+	double simulationTime = 0.0;
+	int tests = 0;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Abilities Active"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Enemy Tower Detection"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Hextech Gunblade"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test In-Game Detection"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Item Actives"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Level Up Dot"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Level Up"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Self Champion"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Shop Available"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Shop Items"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Shop Items 2"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Shop Open"); tests++;
+
+	simulationTime += runTestAndOutput("Resources/Test Images/", "Test Used Potion"); tests++;
+
+	printf("\n");
+	printf("• Finished %d tests...\n", tests);
+	printf("• Average analysis time: %g\n", simulationTime / tests);
+	printf("• About %d frames per second\n", (int)(1.0/(simulationTime / tests)));
+	printf("\n");
+	return(0);
+}
+
+double runTestAndOutput(string folder, string pngImageName) {
+	printf("-------------------\n");
+	printf(("Starting detection on %s\n"), (pngImageName + ".png").c_str());
+	ImageData image = loadImage(folder + pngImageName + ".png");
 
 	DetectionManager* detection = new DetectionManager();
 
@@ -22,17 +63,17 @@ int main() {
 
 
 	printDetected(detection);
-	printf("-------------------\n");
-	printf("Finished detection in %g seconds!\n", simulation_time);
-
+	
+	printf("Finished single image detection in %g seconds!\n", simulation_time);
+printf("-------------------\n");
 	CImg<unsigned char> render1(image.imageWidth,image.imageHeight,1,4);
 	render1.fill(0);
-	outputImage(image, detection, render1, "Output.png");
+	outputImage(image, detection, render1, "Output/Output "+ pngImageName + ".png");
 
-	CImg<unsigned char> render2("Test1.png");
-	outputImage(image, detection, render2, "OutputOverlay.png");
+	CImg<unsigned char> render2((folder + pngImageName + ".png").c_str());
+	outputImage(image, detection, render2, "Output/Output "+pngImageName + " Overlay.png");
 
-	return(0);
+	return simulation_time;
 }
 
 void outputImage(ImageData image, DetectionManager* detectionManager, CImg<unsigned char> render, string name) {
@@ -105,90 +146,69 @@ void outputImage(ImageData image, DetectionManager* detectionManager, CImg<unsig
  		render.draw_line(champion->topLeft.x, champion->topLeft.y, champion->bottomLeft.x, champion->bottomLeft.y,color);
  		render.draw_line(champion->topRight.x, champion->topRight.y, champion->bottomRight.x, champion->bottomRight.y,color);
 	}
-
-	if (spell1LevelUpAvailable) {
-//spell1LevelUp
-	}
-	if (spell2LevelUpAvailable) {
-
-	}
-	if (spell3LevelUpAvailable) {
-
-	}
-	if (spell4LevelUpAvailable) {
-
-	}
-	//spell1LevelDots
-	//spell2LevelDots
-	//spell3LevelDots
-	//spell4LevelDots;
-	if (spell1ActiveAvailable) {
-//spell1Active
-	}
-	if (spell2ActiveAvailable) {
-
-	}
-	if (spell3ActiveAvailable) {
-
-	}
-	if (spell4ActiveAvailable) {
-
-	}
-	if (summonerSpell1ActiveAvailable) {
-//summonerSpell1Active
-	}
-	if (summonerSpell2ActiveAvailable) {
-
-	}
-	if (trinketActiveAvailable) {
-		//trinketActive
-	}
-	if (item1ActiveAvailable) {
-		//item1Active
-	}
-	if (item2ActiveAvailable) {
-	}
-	if (item3ActiveAvailable) {
-	}
-	if (item4ActiveAvailable) {
-	}
-	if (item5ActiveAvailable) {
-	}
-	if (item6ActiveAvailable) {
-	}
-	if (potionActiveAvailable) {
-//potionActive
-	}
-	if (potionBeingUsedShown) {
-		//potionBeingUsed
-	}
-	if (shopAvailableShown) {
-		//shopAvailable
-	}
-	if (shopTopLeftCornerShown) {
-//shopTopLeftCorner
-	}
-	if (shopBottomLeftCornerShown) {
-		//shopBottomLeftCorner
-	}
-	if (detectionManager->buyableItems->size() > 0) {
-
-	}
-	if (mapVisible) {
-//map
-	}
-	if (mapShopVisible) {
-//mapShop
-	}
-	if (mapSelfLocationVisible) {
-		//mapSelfLocation
-	}
-	if (selfHealthBarVisible) {
-		//selfHealthBar
-	}
-	if (surrenderAvailable) {
-		//surrenderActive
-	}
+ /*
+bool getSelfHealthBarVisible();
+    SelfHealth* getSelfHealthBar();
+    bool getSpell1LevelUpVisible();
+    bool getSpell2LevelUpVisible();
+    bool getSpell3LevelUpVisible();
+    bool getSpell4LevelUpVisible();
+    GenericObject* getSpell1LevelUp();
+    GenericObject* getSpell2LevelUp();
+    GenericObject* getSpell3LevelUp();
+    GenericObject* getSpell4LevelUp();
+    std::vector<GenericObject*>* getSpell1LevelDots();
+    std::vector<GenericObject*>* getSpell2LevelDots();
+    std::vector<GenericObject*>* getSpell3LevelDots();
+    std::vector<GenericObject*>* getSpell4LevelDots();
+    int getCurrentLevel();
+    bool getSpell1Available();
+    bool getSpell2Available();
+    bool getSpell3Available();
+    bool getSpell4Available();
+    GenericObject* getSpell1();
+    GenericObject* getSpell2();
+    GenericObject* getSpell3();
+    GenericObject* getSpell4();
+    bool getSummonerSpell1Available();
+    bool getSummonerSpell2Available();
+    GenericObject* getSummonerSpell1();
+    GenericObject* getSummonerSpell2();
+    bool getTrinketActiveAvailable();
+    GenericObject* getTrinketActive();
+    bool getItem1ActiveAvailable();
+    bool getItem2ActiveAvailable();
+    bool getItem3ActiveAvailable();
+    bool getItem4ActiveAvailable();
+    bool getItem5ActiveAvailable();
+    bool getItem6ActiveAvailable();
+    GenericObject* getItem1Active();
+    GenericObject* getItem2Active();
+    GenericObject* getItem3Active();
+    GenericObject* getItem4Active();
+    GenericObject* getItem5Active();
+    GenericObject* getItem6Active();
+    bool getPotionActiveAvailable();
+    GenericObject* getPotionActive();
+    bool getPotionBeingUsedVisible();
+    GenericObject* getPotionBeingUsed();
+    bool getShopAvailable();
+    GenericObject* getShopAvailableObject();
+    bool getShopTopLeftCornerVisible();
+    GenericObject* getShopTopLeftCorner();
+    bool getShopBottomLeftCornerVisible();
+    GenericObject* getShopBottomleftCorner();
+    std::vector<GenericObject*>* getBuyableItems();
+    bool getMapVisible();
+    GenericObject* getMap();
+    bool getMapShopVisible();
+    GenericObject* getMapShop();
+    bool getMapLocationVisible();
+    GenericObject* getMapLocation();
+    bool getSurrenderAvailable();
+    GenericObject* getSurrender();
+    int getPotionActiveItemSlot();
+ */
 
 	render.save_png(name.c_str());
 }
